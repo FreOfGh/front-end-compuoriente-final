38a756814a68ffac56339426a1733e883e68ec1c
apodex <apodexsoftware@gmail.com>
2026-04-07 16:42:47 -0500

    f

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "compuoriente-final-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {

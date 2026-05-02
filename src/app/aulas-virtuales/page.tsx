1a13a120cdd023fb2133e53a0f459def35da3dce
Valentin Sanders <125939436+FreOfGh@users.noreply.github.com>
2026-05-02 14:05:33 -0500

    Add 'recording' option to actions list

--- src/app/aulas-virtuales/page.tsx
+++ src/app/aulas-virtuales/page.tsx
@@ -171,6 +171,7 @@ export default function AulasVirtualesPage() {
             "tileview",
             "hangup",
             "fullscreen",
+            "recording",
           ],
         },
       });
